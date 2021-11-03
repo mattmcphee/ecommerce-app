fa482e0d9cc32d4e2edeca9e77d7f8b766050161
MattMcPhee <matt.imcphee@gmail.com>
2021-11-03 13:58:44 +1000

    Added changes for heroku deployment

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth');
-const adminProductsRouter = require('./routes/admin/products');
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
+const express = require('express')
+const cookieSession = require('cookie-session')
+const authRouter = require('./routes/admin/auth')
+const adminProductsRouter = require('./routes/admin/products')
+const productsRouter = require('./routes/products')
+const cartsRouter = require('./routes/carts')
 
-const app = express();
+const app = express()
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
 app.use(
   cookieSession({
     keys: ['lkasld235j']
   })
-);
-app.use(authRouter);
-app.use(productsRouter);
-app.use(adminProductsRouter);
-app.use(cartsRouter);
+)
+app.use(authRouter)
+app.use(productsRouter)
+app.use(adminProductsRouter)
+app.use(cartsRouter)
 
-app.listen(3000, () => {
-  console.log('Listening');
-});
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...')
+})
